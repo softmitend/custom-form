36e0f5c174ccba2e07b1c96a75088af96a8af77a
alvinaaulia <alvinaaulians@gmail.com>
2026-07-23 21:52:23 +0700

    Fix mixed content security warning

---
+++ app/Http/Middleware/AddSecurityHeaders.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use Illuminate\Http\Request;
+use Symfony\Component\HttpFoundation\Response;
+
+class AddSecurityHeaders
+{
+    public function handle(Request $request, Closure $next): Response
+    {
+        /** @var Response $response */
+        $response = $next($request);
+
+        if (app()->environment('production')) {
+            $response->headers->set('Content-Security-Policy', 'upgrade-insecure-requests');
+            $response->headers->set('X-Content-Type-Options', 'nosniff');
+            $response->headers->set('Referrer-Policy', 'strict-origin-when-cross-origin');
+        }
+
+        return $response;
+    }
+}
--- config/session.php
+++ config/session.php
@@ -169,7 +169,7 @@ return [
     |
     */
 
-    'secure' => env('SESSION_SECURE_COOKIE', env('APP_ENV') === 'production'),
+    'secure' => env('APP_ENV') === 'production' || env('SESSION_SECURE_COOKIE', false),
 
     /*
     |--------------------------------------------------------------------------
