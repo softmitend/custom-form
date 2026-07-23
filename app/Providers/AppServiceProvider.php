816957e6d453095b6c3b20ba43d710ccc84f7e26
alvinaaulia <alvinaaulians@gmail.com>
2026-07-23 20:38:04 +0700

    Use relative Vite asset paths

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,6 +2,7 @@
 
 namespace App\Providers;
 
+use Illuminate\Foundation\Vite;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -19,6 +20,8 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        //
+        $this->app->make(Vite::class)->createAssetPathsUsing(
+            fn (string $path): string => '/'.ltrim($path, '/'),
+        );
     }
 }
