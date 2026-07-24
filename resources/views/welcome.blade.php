47f6ebb6d4168d9a9500eb0e52c3402868138913
alvinaaulia <alvinaaulians@gmail.com>
2026-07-24 20:19:33 +0700

    change web browser tab name

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>Custom Web Form Studio</title>
+    <title>MadeYours</title>
     @vite(['resources/css/app.css', 'resources/js/app.js'])
 </head>
 <body>
